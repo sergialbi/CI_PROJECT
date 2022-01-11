6ddd26ba34f850b42be76a53643d645a7f05424b
BenetManzanaresSalor <benetmanzanares@gmail.com>
2022-01-11 15:41:47 +0100

    DQN with Epsilon for random exploratory actions

--- constants/constants_dqn.py
+++ constants/constants_dqn.py
@@ -9,4 +9,7 @@ BATCH_SIZE = 32
 MEMORY_SIZE = BATCH_SIZE*1000
 GAMMA = 0.9
 EPOCHS_PER_TARGET_UPD = 1
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
